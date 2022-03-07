6eb8df4ff107bc19379cbeb05c610b52481a45ef
Akshara Pramod <akshararuhi@gmail.com>
2022-03-07 23:14:45 +0530

    Change to EJS

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express=require("express")
 const bodyParser=require("body-parser")
+const ejs=require("ejs");
 
 const app=express();
 app.use(express.static("public"));
-
+app.set('view engine', 'ejs');
 app.get("/", function(req,res)
 {
-    res.sendFile(__dirname+"/index.html")
+    res.render("index");
 })
+
+app.get("/monitor", function(req,res)
+{
+    
+    res.render("monitor");
+})
+
+
+
 
 app.listen(3000)
